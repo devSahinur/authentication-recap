ede2fda9db903c401ae67ed44e56c512889b93db
Sahinur Islam <infosahinur@gmail.com>
2021-03-22 11:48:49 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,92 @@
+import logo from './logo.svg';
+import firebase from "firebase/app";
+import "firebase/auth";
+import './App.css';
+import firebaseConfig from './firebase.config';
+import { useState } from 'react';
+
+
+
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
+}else {
+  firebase.app(); // if already initialized, use that one
+}
+
+function App() {
+  const [user, setUser] = useState({});
+
+  var provider = new firebase.auth.GoogleAuthProvider();
+  var fbProvider = new firebase.auth.FacebookAuthProvider();
+  var ghProvider = new firebase.auth.GithubAuthProvider();
+
+
+  const handleGoogleSignIn = () => {
+    firebase.auth().signInWithPopup(provider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email);
+      });
+  };
+
+  const handleFacebookSignIn = () => {
+    firebase.auth().signInWithPopup(fbProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var user = result.user;
+        var accessToken = credential.accessToken;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email);
+      });
+  };
+
+  const handleGitHubSignIn = () => {
+    firebase.auth().signInWithPopup(ghProvider)
+      .then((result) => {
+        var credential = result.credential;
+        var token = credential.accessToken;
+        var user = result.user;
+        console.log(user);
+        setUser(user);
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        var email = error.email;
+        var credential = error.credential;
+        console.log(errorCode, errorMessage, email);
+      });
+  };
+
+  return ( 
+    <div className = "App" >
+     <button onClick ={handleGoogleSignIn}>Sign in using Google</button>
+     <br/>
+     <button onClick ={handleFacebookSignIn}>Sign in using Facebook</button>
+     <br/>
+     <button onClick ={handleGitHubSignIn}>Sign in using GitHub</button>
+      <h3>Email: {user.email}</h3>
+      <h3>Your Name : {user.displayName}</h3>
+      <img src={user.photoURL} alt=""/>
+    </div>
+  );
+}
+
+export default App;
